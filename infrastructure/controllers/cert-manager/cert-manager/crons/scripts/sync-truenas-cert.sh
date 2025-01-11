c7aac99bf923a06cf989a0463972eb7b7c1b1160
dan <dan@omux.dev>
2025-01-11 14:04:18 +0000

    Feat: Add cert-manager cron for truenas server

---
+++ infrastructure/controllers/cert-manager/cert-manager/crons/scripts/sync-truenas-cert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the certificate from the kubernetes secret
+kubectl get secret -n cert-manager truenas-tls -o jsonpath='{.data.tls\.crt}' | base64 -d > /tmp/truenas.crt
+kubectl get secret -n cert-manager truenas-tls -o jsonpath='{.data.tls\.key}' | base64 -d > /tmp/truenas.key
+
+# Copy to TrueNAS using the API
+curl -k -X POST \
+  "https://tank.omux.io/api/v2.0/certificate" \
+  -H "Authorization: Bearer YOUR_API_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "letsencrypt",
+    "certificate": "'$(cat /tmp/truenas.crt | sed 's/$/\\n/' | tr -d '\n')'",
+    "privatekey": "'$(cat /tmp/truenas.key | sed 's/$/\\n/' | tr -d '\n')'"
+  }'
+
+# Clean up
+rm /tmp/truenas.crt /tmp/truenas.key
