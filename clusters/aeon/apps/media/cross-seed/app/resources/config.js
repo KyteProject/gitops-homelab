94a8e5dd23ce6ddfb955d8f2f64faf7d68492522
dan <dan@omux.dev>
2025-03-30 18:46:45 +0100

    Revert "move cross-seed config into externalsecret"
    This reverts commit 95c5d4454e64dc93705a05682398cdc1813db74e.

---
+++ clusters/aeon/apps/media/cross-seed/app/resources/config.js
@@ -0,0 +1,31 @@
+// Torrent content layout: Original
+// Default Torrent Management Mode: Automatic
+// Default Save Path: /media/downloads/torrents/complete
+// Incomplete Save Path: /media/downloads/torrents/incomplete
+
+module.exports = {
+  action: "inject",
+  apiKey: process.env.CROSS_SEED_API_KEY,
+  delay: 30,
+  includeNonVideos: true,
+  includeSingleEpisodes: true,
+  linkCategory: "cross-seed",
+  linkDirs: [
+    "/media/torrents/audiobooks",
+    "/media/torrents/anime",
+    "/media/torrents/books",
+    "/media/torrents/games",
+    "/media/torrents/movies",
+    "/media/torrents/music",
+    "/media/torrents/software",
+    "/media/torrents/tv"
+  ],
+  linkType: "hardlink",
+  matchMode: "partial",
+  outputDir: "/tmp",
+  port: Number(process.env.CROSS_SEED_PORT),
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
+  skipRecheck: true,
+  torznab: [], // Using autobrr for announcements
+  useClientTorrents: true,
+};
