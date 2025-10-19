220b520bb1f5b6542beae0fcbac0cf087abbeef4
dan <dan@omux.dev>
2025-10-19 13:28:37 +0100

    update cross-seed config

--- clusters/aeon/apps/media/cross-seed/app/resources/config.js
+++ clusters/aeon/apps/media/cross-seed/app/resources/config.js
@@ -23,19 +23,19 @@ module.exports = {
   skipRecheck: true,
   torrentClients: ["qbittorrent:http://qbittorrent.media.svc.cluster.local"],
   radarr: [
-    "http://radarr.media.svc.cluster.local:7878/?apikey=" + process.env.RADARR_API_KEY
+    // "http://radarr.media.svc.cluster.local:7878/?apikey=" + process.env.RADARR_API_KEY
   ],
   skipRecheck: true,
   sonarr: [
-    "http://sonarr.media.svc.cluster.local:8989/?apikey=" + process.env.SONARR_API_KEY
+    // "http://sonarr.media.svc.cluster.local:8989/?apikey=" + process.env.SONARR_API_KEY
   ],
   torznab: [
-    'http://prowlarr.media.svc.cluster.local:9696/1/api?apikey=' + process.env.PROWLARR_API_KEY,
-    'http://prowlarr.media.svc.cluster.local:9696/2/api?apikey=' + process.env.PROWLARR_API_KEY,
-    'http://prowlarr.media.svc.cluster.local:9696/3/api?apikey=' + process.env.PROWLARR_API_KEY,
-    'http://prowlarr.media.svc.cluster.local:9696/36/api?apikey=' + process.env.PROWLARR_API_KEY,
-    'http://prowlarr.media.svc.cluster.local:9696/35/api?apikey=' + process.env.PROWLARR_API_KEY,
-    'http://prowlarr.media.svc.cluster.local:9696/69/api?apikey=' + process.env.PROWLARR_API_KEY
+    // 'http://prowlarr.media.svc.cluster.local:9696/1/api?apikey=' + process.env.PROWLARR_API_KEY,
+    // 'http://prowlarr.media.svc.cluster.local:9696/2/api?apikey=' + process.env.PROWLARR_API_KEY,
+    // 'http://prowlarr.media.svc.cluster.local:9696/3/api?apikey=' + process.env.PROWLARR_API_KEY,
+    // 'http://prowlarr.media.svc.cluster.local:9696/36/api?apikey=' + process.env.PROWLARR_API_KEY,
+    // 'http://prowlarr.media.svc.cluster.local:9696/35/api?apikey=' + process.env.PROWLARR_API_KEY,
+    // 'http://prowlarr.media.svc.cluster.local:9696/69/api?apikey=' + process.env.PROWLARR_API_KEY
   ],
   useClientTorrents: true,
 };
